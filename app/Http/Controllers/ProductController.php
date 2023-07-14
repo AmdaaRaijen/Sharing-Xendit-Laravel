41adaa67df35502aaba988904d38cf93f40f3ae9
amda23 <bintangtriadmaja@gmail.com>
2023-07-14 15:20:05 +0700

    create product index and add it to controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render('Product/Index');
+    }
 }
