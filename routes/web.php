6e18c28603beb10eb71b980e90db52b02917ca19
amda23 <bintangtriadmaja@gmail.com>
2023-07-14 15:21:52 +0700

    create route for product controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -32,4 +33,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+    Route::controller(ProductController::class)->prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    });
 });
