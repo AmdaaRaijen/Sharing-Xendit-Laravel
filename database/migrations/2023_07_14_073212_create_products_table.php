19fc66d95961ff4c8cdbaffb5bd66f77c9872237
amda23 <bintangtriadmaja@gmail.com>
2023-07-14 14:44:51 +0700

    fix error in product migration

--- database/migrations/2023_07_14_073212_create_products_table.php
+++ database/migrations/2023_07_14_073212_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->int('price');
+            $table->integer('price');
             $table->string('image');
             $table->timestamps();
         });
